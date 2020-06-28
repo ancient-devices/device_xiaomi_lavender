86178ebfaa1195b4f60016b3d13eb869ba913609
Genkzsz11 <Genkzsz11@gmail.com>
2020-06-28 23:40:10 +0700

    lavender: add prebuilts kimcirecorder
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- device.mk
+++ device.mk
@@ -347,6 +347,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
+# Recorder
+PRODUCT_PACKAGES += \
+    KimciRecorder
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
