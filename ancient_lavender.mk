b76c6378eb6dff0e35c1a8bb4fe109a4ccee133c
AbrarNoob <hasinabrarhaque@gmail.com>
2021-09-25 21:50:57 +0600

    lavender: update coral fp to sep 21

--- ancient_lavender.mk
+++ ancient_lavender.mk
@@ -30,6 +30,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="lavender" \
     PRODUCT_NAME="lavender" \
-    PRIVATE_BUILD_DESC="coral-user 11 RQ3A.210705.001 7380771 release-keys"
+    PRIVATE_BUILD_DESC="coral-user 11 RQ3A.210905.001 7511028 release-keys"
 
-BUILD_FINGERPRINT :="google/coral/coral:11/RQ3A.210705.001/7380771:user/release-keys"
+BUILD_FINGERPRINT :="google/coral/coral:11/RQ3A.210905.001/7511028:user/release-keys"
