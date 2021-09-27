3d85ac8b5f32811344451221eb16cf762aeab5d2
AbrarNoob <hasinabrarhaque@gmail.com>
2021-09-27 19:54:14 +0600

    lavender: build livedisplay

--- device.mk
+++ device.mk
@@ -22,7 +22,7 @@ TARGET_SCREEN_WIDTH := 1080
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-ppui
+    $(LOCAL_PATH)/overlay-lineage
 
 # RRO configuration
 PRODUCT_ENFORCE_RRO_TARGETS := *
