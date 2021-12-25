36183c0fee1e9c2a972a93ab0691eccfad809456
AbrarNoob <hasinabrarhaque@gmail.com>
2021-12-25 11:44:11 +0600

    welcome ancient

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/octavi_lavender.mk
+    $(LOCAL_DIR)/ancient_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    octavi_lavender-user \
-    octavi_lavender-userdebug \
-    octavi_lavender-eng
+    ancient_lavender-user \
+    ancient_lavender-userdebug \
+    ancient_lavender-eng
